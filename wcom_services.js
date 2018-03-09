ce16347b8012ac071ff3c619c9fe1e47a620985c
Honchar Denys <crackeraki@gmail.com>
2018-03-09 11:43:10 +0200

    added search scripts for mongo service

--- wcom_services.js
+++ wcom_services.js
@@ -157,14 +157,14 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 		$http.get('/waw/newId').then(function(resp){
 			cb(resp.data);
 		});
-	}
+	};
 	var replace = function(doc, value, rpl){
 		if(typeof rpl == 'function'){
 			rpl(doc[value], function(newValue){
 				doc[value] = newValue;
 			}, doc);
 		}
-	}
+	};
 	this.push = function(part, doc, rpl){
 		if(rpl){
 			for(var key in rpl){
@@ -172,7 +172,7 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 			}
 		}
 		Array.isArray(self.cl[part])&&self.cl[part].push(doc);
-	}
+	};
 	this.unshift = function(part, doc, rpl){
 		if(rpl){
 			for(var key in rpl){
@@ -180,7 +180,7 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 			}
 		}
 		Array.isArray(self.cl[part])&&self.cl[part].unshift(doc);
-	}
+	};
 	this.get = function(part, rpl, opts, cb){
 		if(typeof rpl == 'function') cb = rpl;
 		if(typeof opts == 'function') cb = opts;
@@ -209,7 +209,7 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 			console.log(err);
 		});
 		return self.cl[part];
-	}
+	};
 	this.use = function(part, cb){
 		if(!self.clpc[part]){
 			return $timeout(function(){
@@ -217,7 +217,7 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 			}, 250);
 		}
 		return cb&&cb(self.cl[part]);
-	}
+	};
 	this.run = function(parts, cb){
 		if(Array.isArray(parts)){
 			for (var i = 0; i < parts.length; i++) {
@@ -235,7 +235,7 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 			}
 		}
 		cb();
-	}
+	};
 	this.fill = function(obj, fromPart, toField, fields, cb){
 		if(typeof fields == 'function') cb = fields;
 		if(!self.clpc[fromPart]){
@@ -269,7 +269,7 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 			}
 		}
 		cb&&cb();
-	}
+	};
 	this.populate = function(toPart, fromPart, toField, fields, cb){
 		if(typeof fields == 'function') cb = fields;
 		if(!self.clpc[toPart]||!self.clpc[fromPart]){
@@ -309,7 +309,7 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 			}
 		}
 		cb&&cb();
-	}
+	};
 	this.create = function(part, obj, cb){
 		$http.post('/api/'+part+'/create', obj||{})
 		.then(function(resp){
@@ -319,7 +319,7 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 				cb(false);
 			}
 		});
-	}
+	};
 	this.update = function(part, obj, custom, cb){
 		if(typeof custom == 'function') cb = custom;
 		if(typeof custom != 'string') custom = '';
@@ -334,34 +334,34 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 				cb(false);
 			}
 		});
-	}
+	};
 	this.updateAfterWhile = function(part, obj, cb){
 		$timeout.cancel(obj.updateTimeout);
 		obj.updateTimeout = $timeout(function(){
 			self.update(part, obj, cb);
 		}, 1000);
-	}
+	};
 	this.updateAll = function(part, obj, custom, cb){
 		if(typeof custom == 'function') cb = custom;
 		if(typeof custom != 'string') custom = '';
-		$http.post('/api/'+part+'/update/all'+(obj._name||''), obj).then(function(resp){
+		$http.post('/api/'+part+'/update/all'+custom, obj).then(function(resp){
 			if(resp.data&&typeof cb == 'function'){
 				cb(resp.data);
 			}else if(typeof cb == 'function'){
 				cb(false);
 			}
 		});
-	}
+	};
 	this.updateAfterWhileAll = function(part, obj, cb){
 		$timeout.cancel(obj.updateTimeout);
 		obj.updateTimeout = $timeout(function(){
 			self.updateAll(part, obj, cb);
 		}, 1000);
-	}
+	};
 	this.afterWhile = function(obj, cb, time){
 		$timeout.cancel(obj.updateTimeout);
 		obj.updateTimeout = $timeout(cb, time||1000);
-	}
+	};
 	this.delete = function(part, obj, custom, cb){
 		if(typeof custom == 'function') cb = custom;
 		if(typeof custom != 'string') custom = '';
@@ -384,28 +384,132 @@ angular.module("wcom_services", []).run(function($rootScope, $compile){
 				cb(false);
 			}
 		});
-	}
+	};
 	this.inDocs = function(doc, docs){
 		for (var i = 0; i < docs.length; i++) {
 			if(docs[i]._id == doc._id) return true;
 		}
 		return false;
-	}
+	};
 	this.c_text = function(text, clear){
 		text = text.split(clear||' ');
 		for (var i = text.length - 1; i >= 0; i--) {
 			if(text[i]=='') text.splice(i, 1);
 		}
 		return text.join('');
-	}
+	};
 	// doc fill
 	this.beArray = function(val, cb){
 		if(!Array.isArray(val)) cb([]);
 		else cb(val);
-	}
+	};
 	this.forceObj = function(val, cb){
 		cb({});
-	}
+	};
+	// search in docs
+	this.keepByBiggerNumber = function(docs, field, number){
+		for (var i = docs.length - 1; i >= 0; i--) {
+			if(Array.isArray(docs[i][field])){
+				let keep = false;
+				for (var j = 0; j < docs[i][field].length; j++) {
+					if (docs[i][field][j] >= number) {
+						keep = true;
+						break;
+					}
+				}
+				if(keep) continue;
+			}else{
+				if(docs[i][field] >= number){
+					continue;
+				}
+			}
+			docs.splice(i, 1);
+		}
+	};
+	this.keepBySmallerNumber = function(docs, field, number){
+		for (var i = docs.length - 1; i >= 0; i--) {
+			if(Array.isArray(docs[i][field])){
+				let keep = false;
+				for (var j = 0; j < docs[i][field].length; j++) {
+					if (docs[i][field][j] <= number) {
+						keep = true;
+						break;
+					}
+				}
+				if(keep) continue;
+			}else{
+				if(docs[i][field] <= number){
+					continue;
+				}
+			}
+			docs.splice(i, 1);
+		}
+	};
+	this.cutByBiggerNumber = function(docs, field, number){};
+	this.cutBySmallerNumber = function(docs, field, number){};
+	this.keepByText = function(docs, field, string, equal){
+		string = string.toLowerCase();
+		for (var i = docs.length - 1; i >= 0; i--) {
+			if(Array.isArray(docs[i][field])){
+				let keep = false;
+				for (var j = 0; j < docs[i][field].length; j++) {
+					if (equal) {
+						if (docs[i][field][j].toLowerCase() == string) {
+							keep = true;
+							break;
+						}
+					} else {
+						if (docs[i][field][j].toLowerCase().indexOf(string)>-1) {
+							keep = true;
+							break;
+						}
+					}
+				}
+				if(keep) continue;
+			}else{
+				if(equal){
+					if(docs[i][field].toLowerCase() == string){
+						continue;
+					}
+				}else{
+					if(docs[i][field].toLowerCase().indexOf(string)>-1){
+						continue;
+					}
+				}
+			}
+			docs.splice(i, 1);
+		}
+	};
+	this.cutByText = function(docs, field, string, equal){
+		string = string.toLowerCase();
+		for (var i = docs.length - 1; i >= 0; i--) {
+			if(Array.isArray(docs[i][field])){
+				for (var j = 0; j < docs[i][field].length; j++) {
+					if (equal) {
+						if (docs[i][field][j].toLowerCase() == string) {
+							docs.splice(i, 1);
+							break;
+						}
+					} else {
+						if (docs[i][field][j].toLowerCase().indexOf(string)>-1) {
+							docs.splice(i, 1);
+							break;
+						}
+					}
+				}
+			}else{
+				if(equal){
+					if(docs[i][field].toLowerCase() == string){
+						docs.splice(i, 1);
+					}
+				}else{
+					if(docs[i][field].toLowerCase().indexOf(string)>-1){
+						docs.splice(i, 1);
+					}
+				}
+			}
+		}
+	};
 }).service('img', function(){
 	"ngInject";
 	this.fileToDataUrl = function(file, callback){
